806fc39dd4010774805d7523c6c89b060d1f04df
DESKTOP-GQKH5RS\Ybr <podonin.developer@gmail.com>
2018-02-21 01:08:03 +0300

    initial commit

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
@@ -0,0 +1,28 @@
+package com.bignerdranch.android.criminalintent;
+
+import java.util.UUID;
+
+/**
+ * Created by Ybr on 20.02.2018.
+ */
+
+public class Crime {
+    private UUID mId;
+    private String mTitle;
+
+    public Crime(){
+        mId = UUID.randomUUID();
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+}
